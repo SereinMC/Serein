f6c386f9e1fc381e69442eb2c9c6a752095049f6
BuShe Pie <bushe.cn@icloud.com>
2023-02-02 05:02:10 +0000

    ci: vscode formatter is a lie

--- tools/deploy-raw.js
+++ tools/deploy-raw.js
@@ -3,21 +3,21 @@
 const https = require('node:https');
 
 const options = {
-    hostname: 'api.cloudflare.com',
-    port: 443,
-    path: process.env.DEPLOY_API,
-    method: 'POST',
+	hostname: 'api.cloudflare.com',
+	port: 443,
+	path: process.env.DEPLOY_API,
+	method: 'POST',
 };
 
 const req = https.request(options, (res) => {
-    console.log('statusCode:', res.statusCode);
+	console.log('statusCode:', res.statusCode);
 
-    res.on('data', (d) => {
-        process.stdout.write(d);
-    });
+	res.on('data', (d) => {
+		process.stdout.write(d);
+	});
 });
 
 req.on('error', (e) => {
-    console.error(e);
+	console.error(e);
 });
 req.end();
