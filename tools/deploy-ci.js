7cb78e3a5cb3829ffa8b1f334d7936f29c25822d
BuShe Pie <bushe.cn@icloud.com>
2023-02-02 04:52:29 +0000

    ci: add `raw-ci` workflow

---
+++ tools/deploy-ci.js
@@ -0,0 +1,23 @@
+#!/usr/bin/env node
+// Usage: tools/deploy-ci.js
+const https = require('node:https');
+
+const options = {
+    hostname: 'api.cloudflare.com',
+    port: 443,
+    path: process.env.DEPLOY_API,
+    method: 'POST',
+};
+
+const req = https.request(options, (res) => {
+    console.log('statusCode:', res.statusCode);
+
+    res.on('data', (d) => {
+        process.stdout.write(d);
+    });
+});
+
+req.on('error', (e) => {
+    console.error(e);
+});
+req.end();
