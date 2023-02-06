17206828306263071d7aeaca13fcc1f31f43a3c1
BuShe Pie <bushe.cn@icloud.com>
2023-02-06 23:38:49 +0800

    ci: new `minecraft-deps` workflow

---
+++ tools/deps/minecraft-deps.js
@@ -0,0 +1,43 @@
+import axios from 'axios';
+import fs from 'fs';
+
+const packageNames = ['@minecraft/server', '@minecraft/server-ui', '@minecraft/server-gametest'];
+const npmVersionFile = 'npm_version.json';
+
+async function getLatestVersion(packageName: string) {
+  try {
+    const { data } = await axios.get(`https://registry.npmjs.org/${packageName}/latest`);
+    return data.version;
+  } catch (error) {
+    console.error(`Error getting latest version of ${packageName}: ${error}`);
+    return null;
+  }
+}
+
+async function updatePackageVersion(packageName: string, version: string) {
+  try {
+    const fileData = JSON.parse(fs.readFileSync(npmVersionFile, 'utf8'));
+    if (fileData[packageName] === version) return;
+
+    fileData[packageName] = version;
+    fs.writeFileSync(npmVersionFile, JSON.stringify(fileData, null, 2));
+    console.log(`Successfully updated ${packageName} version to ${version} in ${npmVersionFile}`);
+  } catch (error) {
+    console.error(`Error updating ${packageName} version in ${npmVersionFile}: ${error}`);
+  }
+}
+
+async function main() {
+  try {
+    const fileData = JSON.parse(fs.readFileSync(npmVersionFile, 'utf8'));
+    for (const packageName of packageNames) {
+      const latestVersion = await getLatestVersion(packageName);
+      if (!latestVersion) continue;
+      updatePackageVersion(packageName, latestVersion);
+    }
+  } catch (error) {
+    console.error(`Error reading ${npmVersionFile}: ${error}`);
+  }
+}
+
+main();
