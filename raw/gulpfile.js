da0e01088812a51d2a171764b12507a979c94f7c
东灯 <me@lampese.com>
2024-02-17 02:37:54 +0800

    refactor: remove useless extension action

--- raw/gulpfile.js
+++ raw/gulpfile.js
@@ -207,11 +207,6 @@ const deploy = gulp.series(
 
 const default_action = gulp.series(build, deploy);
 
-async function extension() {
-	const [name, task] = [process.argv[3], process.argv[4]];
-	await (await import(name))[task](config);
-}
-
 export {
 	build,
 	bundle,
@@ -220,5 +215,4 @@ export {
 	default_action as default,
 	watch,
 	compile_scripts as cs,
-	extension
 };
